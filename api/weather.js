1e40399a3fad6a1a026ff80043e774b2f1489910
Prthm77 <prathamp@zignuts.com>
2024-03-05 17:51:44 +0530

    hot-fixes-api

--- api/weather.js
+++ api/weather.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { apiKey } from "../module/api"
+import { apiKey } from "../module/constant"
 
 const forecastEndpoint = params => `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
 const locationsEndpoint = params => `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
